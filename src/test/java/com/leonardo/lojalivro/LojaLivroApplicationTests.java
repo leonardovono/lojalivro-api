e967728c3017aad730d703728a52da0413834be3
leonardovono <leonar.dovono@gmail.com>
2023-07-25 08:48:59 -0300

    commit inicial

---
+++ src/test/java/com/leonardo/lojalivro/LojaLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.lojalivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
